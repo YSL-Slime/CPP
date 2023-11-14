84e329ccc184641a89f611f07291eec818b115e4
YSL-Slime <lulicrok2@gmail.com>
2023-11-14 20:43:39 +0100

    A

---
+++ RPG.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ctime>
+#include <cmath>
+
+enum Races {Orc, Human, Reptile, Fairy, Elf, Dwarf, Goblin, Vampire, Werewolf, Karen}
+enum Classes {Thief, Wizard, Paladin, Knight, Barbarian, Priest, Ranger, Bard}
+
+class World{
+    private:
+        std::string weather;
+        int coordinateX;
+        int coordinateY;
+    public:
+    std::string getWeather()
+}
+
+class Player{
+    
+}
+
+int main(){
+
+}
